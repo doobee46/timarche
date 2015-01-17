7c1ffd12e459fd962a674f6c6d3efb5d6919a4bd
obed lorisson <obed.lorisson@gmail.com>
2015-01-17 06:46:26 +0000

    profile page ui done

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,10 +1,11 @@
 class SellersController < ApplicationController
   def index
-  	@sellers  = User.all
-    @listings = Listing.all
+  	@sellers  = User.all.paginate(:page => params[:page], :per_page => 30)
+    @listings = Listing.all.paginate(:page => params[:page], :per_page => 30)
   end
 
   def show
   	@user = User.find_by_username(params[:id])
+  	@listings = @user.listings.paginate(:page => params[:page], :per_page => 30)
   end
 end

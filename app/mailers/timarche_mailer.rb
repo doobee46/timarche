1e0cd047e9409e1a4ed1cfc2a0dac15953eb22b9
obed lorisson <obed.lorisson@gmail.com>
2015-03-29 18:32:30 +0000

    welcome mailer social media added

--- app/mailers/timarche_mailer.rb
+++ app/mailers/timarche_mailer.rb
@@ -2,6 +2,6 @@ class TimarcheMailer < ActionMailer::Base
   default from: "Equipe@timarche.com"
    def welcome_email(user)
    	 @user = user
-   	 mail(to: @user.email, subject: "welcome")
+   	 mail(to: @user.email, subject: "Bienvenue sur timarché")
    end
  end

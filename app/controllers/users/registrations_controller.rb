3717b7ad26347ec559cf5ab81d5c4f05426316a7
obedlorisson <obed.lorisson@gmail.com>
2015-05-25 00:42:09 +0000

    more fix

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   include ApplicationHelper
 
   def create
-    
+    super
+      # Sends email to user when user is created.
+      TimarcheMailer.welcome_email(@user).deliver
   end
 
   def new

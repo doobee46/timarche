16fb6b6e45ecc91670b16d07c7ff48071e02937d
Lorisson <obed.lorisson@gmail.com>
2018-01-06 05:50:01 +0000

    fix missing bucket option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,9 +103,9 @@ Rails.application.configure do
   storage: :s3,
   s3_region: ENV.fetch('AWS_REGION'),
   s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+    :bucket => ENV.fetch('S3_BUCKET_NAME'),
+    :access_key_id => ENV.fetch('AWS_ACCESS_KEY_ID'),
+    :secret_access_key => ENV.fetch('AWS_SECRET_ACCESS_KEY')
   }
 }
 

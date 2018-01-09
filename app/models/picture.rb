f50069dd70b81a216efdf970f61ac5016a996c7c
Lorisson <obed.lorisson@gmail.com>
2018-01-09 03:02:31 +0000

    fix navbar displaying issue

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,12 +1,8 @@
 class Picture < ActiveRecord::Base
   belongs_to :listing
-  if Rails.env.development?
-    has_attached_file :image, :styles => {:medium => "564x394#" }, :default_url => "default.png"
-  else
-    has_attached_file :image, :styles => {:medium => "564x394#" }, :default_url => "default.png",
-                      :storage => :dropbox,
-                      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
-  end
-   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-   Paperclip.options[:content_type_mappings] = {nil => "image/png"}
+  
+  has_attached_file :image, :styles => { :large=> "564x394#" }, :default_url => "default_:style.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  Paperclip.options[:content_type_mappings] = {nil => "image/png"}
+  
 end
